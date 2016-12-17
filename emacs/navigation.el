9c638e10e6793a1fb13cc97de779b3c4ac7133b7
Patrick Ball <pball@hrdag.org>
2016-12-16 22:19:36 -0800

    trying to get gpg completions, not working

--- emacs/navigation.el
+++ emacs/navigation.el
@@ -1,7 +1,31 @@
 ;;; PB's navigation
 
 ;; ace-window keys
+
 (use-package ace-window
+  :ensure t
   :init
-  (setq aw-keys '(?q ?w ?e ?r ?a ?s ?d ?f))
-  :bind ("C-'" . ace-window))
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
+    )
+  :bind ("C-'" . ace-window)
+  :config (progn (setq
+		  aw-keys '(?f ?g ?h ?j ?k ?l)
+		  aw-background nil))
+  )
+
+;; (use-package ace-window
+;;   :ensure t
+;;   :config 
+;;   (setq
+;;    aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
+;;    (setq aw-background nil))
+;;   :bind ("C-'" . ace-window))
+;; (custom-set-faces
+;;  '(aw-leading-char-face
+;;    ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
+
