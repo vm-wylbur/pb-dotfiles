93e3315543160eccd185923aa8b6c5c71a8f6f45
Patrick Ball <pball@hrdag.org>
2018-10-10 17:23:25 -0700

    backup working for henwen now

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # based on https://blog.interlinked.org/tutorials/rsync_time_machine.html
-# PB, 2018-10-09
+# PB, 2018-10-10
 # adapted for petunia, henwen
 #
 # configurable backup disk names
 backup_disk_names="backup-buffer|archives|working-archives|archives-2018"
 local_machine_name="henwen"
 
+datestr=$(date "+%Y-%m-%dT%H_%M_%S")
+
 backup_home="${HOME}"
 rsync_opts="--archive --one-file-system --hard-links --delete "
 rsync_opts+="--delete-excluded --safe-links --partial --progress "
 rsync_opts+="--relative"
-# rsync_opts="${rsync_opts} --delete-excluded --safe-links --partial --progress"
-# rsync_opts="${rsync_opts} --relative"
 
 # check for specific volume mounted anywhere.
 backup_mntd=$(ls -1 /Volumes/ | egrep "${backup_disk_names}")
@@ -27,14 +27,15 @@ if [ ! -d "$backup_path" ]; then
     mkdir -p "$backup_path"
 fi
 
-datestr=$(date "+%Y-%m-%dT%H_%M_%S")
+logfile="$HOME/var/log/backup-${datestr}.log"
 backup_excludes="${backup_home}/dotfiles/config/backup-excludes"
+
 backup_current="${backup_path}/current"
 backup_final="${backup_path}/back-${datestr}"
 dirs_to_backup="${backup_home}"
 
 ## this was a bad idea, looking for the incomplete.
-existing_incompletes=$(gfind ${backup_path} -maxdepth 1 -name 'incomplete_back-*')
+existing_incompletes=$(find ${backup_path} -maxdepth 1 -name 'incomplete_back-*')
 if [[ "$existing_incompletes" ]] ; then
     backup_incomplete=$(echo "$existing_incompletes" | gsort | tail -n1)
     echo "using existing incomplete backup: ${backup_incomplete}"
@@ -51,18 +52,22 @@ fi
 # todo: ignore error 24 bc that's just filesystem churn.
 
 echo "Backing up to tmp, will be ${backup_final}"
+set -o xtrace
 rsync $rsync_opts \
       --link-dest="${link_dest}" \
       --exclude-from="${backup_excludes}" \
-      ${backup_home} \
-      ${backup_incomplete}
+      ${HOME} ${backup_incomplete}
 
-if [ $? -eq 0 ]  || [ $? -eq 24 ] ; then
-    mv "${backup_incomplete}" "${backup_final}" \
-	&& rm -f "${backup_current}" \
-	&& ln -sf "${backup_final}" "${backup_current}" \
-	&& rm -rf "${backup_path}/incomplete_back-*" \
-	&& echo "back done: ${backup_final}"
+exitcode=$?
+set +x
+echo "finished rsync with exit code=$exitcode"
+
+if [ $exitcode -eq 0 ]  || [ $exitcode -eq 24 ] ; then
+  mv "${backup_incomplete}" "${backup_final}" \
+    && rm -f "${backup_current}" \
+    && ln -sf "${backup_final}" "${backup_current}" \
+    && rm -rf "${backup_path}/incomplete_back-*" \
+    && echo "back done: ${backup_final}"
 else
     echo "backup FAILED."
 fi
