5c9003bb676cd5da2ce2ddc8cc789404b25d69f1
Patrick Ball <pball@hrdag.org>
2018-10-13 11:13:52 -0700

    trying to make snippets_helper, not working yet

--- scripts/projpath.py
+++ scripts/projpath.py
@@ -3,33 +3,45 @@
 #
 # Authors:     PB
 # Maintainers: PB
-# Copyright:   2017, HRDAG, GPL v2 or later
+# Copyright:   2018, HRDAG, GPL v2 or later
 # ============================================
 # dotfiles/scripts/projpath.py
-# inserted with :r !projpath.py %  
-# could be integrated into a snippet? 
-# 
+# inserted with :r !projpath.py %
+# could be integrated into a snippet?
+#
 import os.path
+import os
 import collections
 import sys
 
-startpath = os.path.realpath(sys.argv[1])
-original_startpath = startpath
-rstack = collections.deque()
-while True:
 
-    lpart, rpart = os.path.dirname(startpath), os.path.basename(startpath)
-    rstack.appendleft(rpart)
+def projpath(startpath):
+    original_startpath = startpath
+    rstack = collections.deque()
+    while True:
 
-    if lpart == '/':
-        raise OSError("no git dir found in {}.".format(original_startpath))
-    subdirs = next(os.walk(lpart))[1]
-    if '.git' in subdirs:
-        rpart = os.path.basename(lpart)
+        lpart, rpart = os.path.dirname(startpath), os.path.basename(startpath)
         rstack.appendleft(rpart)
-        break
-    startpath = lpart
 
-print('# ' + os.path.join(*rstack))
+        if lpart == '/':
+            print('')
+            sys.exit(1)
+            # raise OSError("no git dir found in {}.".format(original_startpath))
 
+        subdirs = next(os.walk(lpart))[1]
+        if '.git' in subdirs:
+            rpart = os.path.basename(lpart)
+            rstack.appendleft(rpart)
+            break
+        startpath = lpart
+
+    return os.path.join(*rstack)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        startpath = os.path.realpath(sys.argv[1])
+    else:
+        startpath = os.getcwd()
+    print(projpath(startpath))
 # done
---
+++ scripts/snippet_helper/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Author: PB
+# Maintainer(s): PB
+# License: (c) HRDAG 2018, GPL v2 or newer
+#
+# filename: dotfiles/scripts/snippet_helper/setup.py
+#
+# -----------------------------------------------------------
+#
+from setuptools import setup
+
+setup(name='snippet_helper',
+      version='0.1',
+      description='helper functions for interpolation in vim snippets',
+      url='http://github.com/vm-wylbur',
+      author='PB',
+      author_email='wylbur@riseup.net',
+      license='MIT',
+      packages=['snippet_helper'],
+      zip_safe=False)
+
+# done.
