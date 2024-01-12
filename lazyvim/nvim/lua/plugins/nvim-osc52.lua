17e284427cd7a23f82aaa0b5046b0e05f36888fe
Patrick Ball <pball@hrdag.org>
2024-01-12 21:41:47 +0000

    setup for scott-only osc52

--- lazyvim/nvim/lua/plugins/nvim-osc52.lua
+++ lazyvim/nvim/lua/plugins/nvim-osc52.lua
@@ -1,31 +1,12 @@
--- only needed on headless linux boxen
+-- TODO: only needed on headless linux boxen
+if vim.fn.hostname() == "scott" then
 return {
   "ojroques/nvim-osc52",
   opts = {
     silent = true, 
   }
-  -- keys = {
-  --   {
-  --     "<leader>y",
-  --     function()
-  --       return require("osc52").copy_operator()
-  --     end,
-  --     desc = "copy selection to system clipboard (normal mode)",
-  --     expr = true,
-  --   },
-  --   {
-  --     "<leader>Y",
-  --     "<leader>y_",
-  --     remap = true,
-  --     desc = "copy current line into system clipboard (normal mode)",
-  --   },
-  --   {
-  --     mode = "v",
-  --     "<leader>y",
-  --     function()
-  --       require("osc52").copy_visual()
-  --     end,
-  --     desc = "copy selection to system clipboard (visual mode)",
-  --   },
-  -- },
 }
+else
+  return { } 
+end 
+-- done.
