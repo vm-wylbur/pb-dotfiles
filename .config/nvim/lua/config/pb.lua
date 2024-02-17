355f35a8d3a38a5fc59ce4b759f0f840a2057a00
Patrick Ball <pball@hrdag.org>
2024-02-16 17:26:22 -0800

    restarting tmux

--- .config/nvim/lua/config/pb.lua
+++ .config/nvim/lua/config/pb.lua
@@ -33,6 +33,9 @@ require("noice").setup({
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+  messages = {
+    enabled = false,
+  },
 })
 
 -- deletes trailing whitespace on save, maintaining cursor position
